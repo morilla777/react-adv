a8fc720a50c1a278914b6c0f28e9a71dcd49f22a
Marco Mora <mmora@pragma.cl>
2024-04-03 15:29:01 -0300

    Homologación con V6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,38 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from 'react-router-dom';
+import { BrowserRouter, NavLink, Navigate, Route, Routes } from "react-router-dom";
 
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 export const Navigation = () => {
+
+  const getActiveLinkClassName = ( isActive: boolean) => {
+    return isActive ? 'nav-active' : '';
+  }
+
   return (
-    <Router>
+    <BrowserRouter>
       <div className="main-layout">
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/home" className={ ({isActive}) => getActiveLinkClassName( isActive ) }>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/about" className={ ({isActive}) => getActiveLinkClassName( isActive ) }>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/users" className={ ({isActive}) => getActiveLinkClassName( isActive ) }>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+        <Routes>
+            <Route path='about' element={<h1>About Page</h1>} />
+            <Route path='users' element={<h1>Users Page</h1>} />
+            <Route path='home' element={<h1>Home Page</h1>} />
+            <Route path='/*' element={ <Navigate to='/home' replace />} />
+        </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
-}
+};
